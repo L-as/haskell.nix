L-as/haskell.nix

commit 9b21af3fcb32ca95c406f9a3779fe125eccff865

Remove unnecessary .. in src

nix-tools/stack2nix/Stack2nix.hs
@@ -119,8 +119,8 @@ packages2nix args (Stack _ _ pkgs) =
               let pkg = cabalFilePkgName cabalFile
                   nix = pkg <.> "nix"
                   nixFile = argOutputDir args </> nix
-                  src = Just . C2N.Path $ relPath </> ".." </> folder
               in do createDirectoryIfMissing True (takeDirectory nixFile)
+                  src = Just . C2N.Path $ relPath </> folder
                     writeDoc nixFile =<<
                       prettyNix <$> cabal2nix src cabalFile
                     return $ fromString pkg $= mkPath False nix

commit 0e5261db7278bab2187886817e310a42094158fe

Fix source lines order broken in 9b21af3

nix-tools/stack2nix/Stack2nix.hs
@@ -119,8 +119,8 @@ packages2nix args (Stack _ _ pkgs) =
               let pkg = cabalFilePkgName cabalFile
                   nix = pkg <.> "nix"
                   nixFile = argOutputDir args </> nix
-              in do createDirectoryIfMissing True (takeDirectory nixFile)
                   src = Just . C2N.Path $ relPath </> folder
+              in do createDirectoryIfMissing True (takeDirectory nixFile)
                     writeDoc nixFile =<<
                       prettyNix <$> cabal2nix src cabalFile
                     return $ fromString pkg $= mkPath False nix