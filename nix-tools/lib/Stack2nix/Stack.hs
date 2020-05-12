L-as/haskell.nix

commit 862036531a4efc61d5f3f64f8d56c4edd4661fe3

Cabal 3.2 support (#86)
* migration to Cabal 3.2

* Use shellFor { tools = ...; }

* Fix compilation with Cabal-3.2

* Update cabal.project

* Update default.nix generated by plan-to-nix

plan-to-nix generated `default.nix` that was incompatible with newer
interface of `haskell.nix` repo.

* Update compiler to ghc883

Co-authored-by: Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
Co-authored-by: Maxim Koltsov <kolmax94@gmail.com>

nix-tools/lib/Stack2nix/Stack.hs
@@ -27,8 +27,11 @@ import Data.Monoid (mempty)
 
 import Distribution.Types.PackageName
 import Distribution.Types.PackageId
-import Distribution.Compat.ReadP hiding (Parser)
 import Distribution.Text
+import Distribution.Pretty
+import Distribution.Parsec
+import Distribution.Compat.CharParsing
+import Distribution.Parsec.FieldLineStream
 import Distribution.Types.Version (nullVersion)
 
 import qualified Data.HashMap.Strict as HM
@@ -101,9 +104,11 @@ newtype CabalRev = CabalRev Int -- cabal revision 0,1,2,...
 type URL      = String -- Git/Hg/... URL
 type Rev      = String -- Git revision
 
-instance Text CabalRev where
-  disp (CabalRev rev) = "r" <> disp rev
-  parse = char 'r' *> (CabalRev <$> parse)
+instance Pretty CabalRev where
+  pretty (CabalRev rev) = "r" <> pretty rev
+
+instance Parsec CabalRev where
+  parsec = char 'r' *> (CabalRev <$> integral)
 
 --------------------------------------------------------------------------------
 -- Data Types
@@ -148,26 +153,26 @@ data Location
 
 --------------------------------------------------------------------------------
 -- Parsers for package indices
-sha256Suffix :: ReadP r Sha256
-sha256Suffix = string "@sha256:" *> many1 (satisfy (`elem` (['0'..'9']++['a'..'z']++['A'..'Z'])))
+sha256Suffix :: ParsecParser Sha256
+sha256Suffix = string "@sha256:" *> some (satisfy (`elem` (['0'..'9']++['a'..'z']++['A'..'Z'])))
                                  -- Stack supports optional cabal file size after revision's SHA value,
                                  -- we parse it but it doesn't get used
-                                 <* optional (char ',' <* many1 (satisfy isDigit))
+                                 <* optional (char ',' <* some (satisfy isDigit))
 
-revSuffix :: ReadP r CabalRev
-revSuffix = string "@rev:" *> (CabalRev . read <$> many1 (satisfy (`elem` ['0'..'9'])))
+revSuffix :: ParsecParser CabalRev
+revSuffix = string "@rev:" *> (CabalRev . read <$> some (satisfy (`elem` ['0'..'9'])))
 
-suffix :: ReadP r (Maybe (Either Sha256 CabalRev))
-suffix = option Nothing (Just <$> (Left <$> sha256Suffix) +++ (Right <$> revSuffix))
+suffix :: ParsecParser (Maybe (Either Sha256 CabalRev))
+suffix = option Nothing (Just <$> ((Left <$> sha256Suffix) <|> (Right <$> revSuffix)))
 
-pkgIndex :: ReadP r Dependency
-pkgIndex = PkgIndex <$> parse <*> suffix <* eof
-
+pkgIndex :: ParsecParser Dependency
+pkgIndex = PkgIndex <$> parsec <*> suffix <* eof
 
 parsePackageIdentifier :: String -> Maybe (PackageIdentifier, Maybe (Either Sha256 CabalRev))
-parsePackageIdentifier input = case readP_to_S pkgIndex input of
-  [(PkgIndex d rev,"")] -> Just (d, rev)
-  _ -> Nothing
+parsePackageIdentifier input =
+  case runParsecParser pkgIndex "<parsePackageIdentifier>" (fieldLineStreamFromString input) of
+    Right (PkgIndex d rev) -> Just (d, rev)
+    _ -> Nothing
 
 --------------------------------------------------------------------------------
 -- JSON/YAML destructors
@@ -209,12 +214,12 @@ instance FromJSON Dependency where
   --       to be ./foo-X.Y.Z
   parseJSON p = parsePkgIndex p <|> parseLocalPath p <|> parseDVCS p
     where parsePkgIndex = withText "Package Index" $ \pi ->
-            case [pi' | (pi',"") <- readP_to_S pkgIndex (T.unpack pi)] of
+            case parsePackageIdentifier (T.unpack pi) of
               -- Cabal will happily parse "foo" as a packageIdentifier,
               -- we however are only interested in those that have a version
               -- as well. Any valid version is larger than @nullVersion@, as
               -- such we can use that as a filter.
-              [pi'@(PkgIndex pkgIdent _)] | pkgVersion pkgIdent > nullVersion -> return $ pi'
+              (Just (pkgIdent, s)) | pkgVersion pkgIdent > nullVersion -> return $ PkgIndex pkgIdent s
               _ -> fail $ "invalid package index: " ++ show pi
           parseLocalPath = withText "Local Path" $
             return . LocalPath . dropTrailingSlash . T.unpack

nix-tools/.buildkite/build.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -I "nixpkgs=channel:nixos-19.09" --pure -i bash -p nix cabal-install ghc git nix-prefetch-git cacert
+#! nix-shell -I "nixpkgs=channel:nixos-20.03" --pure -i bash -p nix cabal-install ghc git nix-prefetch-git cacert
 
-export NIX_PATH="nixpkgs=channel:nixos-19.09"
+export NIX_PATH="nixpkgs=channel:nixos-20.03"
 index_state="2020-01-10T00:00:00Z"
 expected_hash="0z2jc4fibfxz88pfgjq3wk5j3v7sn34xkwb8h60hbwfwhhy63vx6"
 
@@ -12,13 +12,16 @@ set -euo pipefail
 rm -f .nix-tools.cache
 
 echo "+++ Cabal configure"
-cabal new-update "hackage.haskell.org,$index_state"
+cabal new-update
 cabal new-configure
 
 echo
 echo "+++ Run stable version of plan-to-nix"
-nix build '(let haskellNix = builtins.fetchTarball "https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz"; in (import (haskellNix + "/nixpkgs") (import haskellNix)).haskell-nix.nix-tools)' -o nt
+nix build '(let haskellNix = import (builtins.fetchTarball "https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz") {}; in (import haskellNix.sources.nixpkgs-default haskellNix.nixpkgsArgs).haskell-nix.nix-tools)' -o nt
 ./nt/bin/plan-to-nix --output .buildkite/nix1 --plan-json dist-newstyle/cache/plan.json
+
+# Replace currently broken plan-to-nix output
+cp .buildkite/fixed.nix .buildkite/nix1/default.nix
 
 echo
 echo "+++ Build project"
@@ -31,8 +34,8 @@ echo "There are no tests -- https://github.com/input-output-hk/haskell.nix/issue
 echo
 echo "+++ Add runtime dependencies to PATH"
 
-nix build -f channel:nixos-19.03 nix-prefetch-scripts -o nix-prefetch-scripts
-nix build -f channel:nixos-19.03 git -o git
+nix build -f channel:nixos-20.03 nix-prefetch-scripts -o nix-prefetch-scripts
+nix build -f channel:nixos-20.03 git -o git
 export PATH="$PWD/nix-prefetch-scripts/bin:$PWD/git/bin:$PATH"
 
 echo
@@ -45,6 +48,10 @@ rm -f .nix-tools.cache
 nix build -f .buildkite/nix1 nix-tools.components.exes.plan-to-nix
 ./result/bin/plan-to-nix --output .buildkite/nix2 --plan-json dist-newstyle/cache/plan.json
 
+# Add module needed to allow Cabal 3.2 to be installed
+sed -i -e 's|modules = \[\]|modules = \[{ nonReinstallablePkgs = \[ "rts" "ghc-heap" "ghc-prim" "integer-gmp" "integer-simple" "base" "deepseq" "array" "ghc-boot-th" "pretty" "template-haskell" "ghcjs-prim" "ghcjs-th" "ghc-boot" "ghc" "Win32" "array" "binary" "bytestring" "containers" "directory" "filepath" "ghc-boot" "ghc-compact" "ghc-prim" "hpc" "mtl" "parsec" "process" "text" "time" "transformers" "unix" "xhtml" "stm" "terminfo" \]; }\]|' \
+  .buildkite/nix2/default.nix
+
 echo
 echo "+++ Build project"
 
@@ -53,7 +60,10 @@ nix build -f .buildkite/nix2 nix-tools.components.exes --no-link
 echo
 echo "--- Test index file truncation"
 
-find /nix/store/ -name "*-00-index.tar.gz" -exec nix-store --delete {} \;
+shopt -s nullglob
+for a in /nix/store/*-00-index.tar.gz; do nix-store --delete $a; done
+shopt -u nullglob
+
 nix build -f test/truncate-index.nix --no-link \
     --arg nix-tools-path ./.buildkite/nix2  \
     --argstr index-state "$index_state" \

nix-tools/.buildkite/fixed.nix
@@ -1,0 +1,28 @@
+{ haskellNix ? import (builtins.fetchTarball https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz) {}
+, nixpkgs ? haskellNix.sources.nixpkgs-default }:
+
+let
+  pkgs = import nixpkgs haskellNix.nixpkgsArgs;
+
+  pkgSet = pkgs.haskell-nix.mkCabalProjectPkgSet {
+    plan-pkgs = import ./pkgs.nix;
+    pkg-def-extras = [];
+    modules = [{
+     nonReinstallablePkgs= [ "rts" "ghc-heap" "ghc-prim" "integer-gmp" "integer-simple" "base"
+      "deepseq" "array" "ghc-boot-th" "pretty" "template-haskell"
+      # ghcjs custom packages
+      "ghcjs-prim" "ghcjs-th"
+      "ghc-boot"
+      "ghc" "Win32" "array" "binary" "bytestring" "containers"
+      "directory" "filepath" "ghc-boot" "ghc-compact" "ghc-prim"
+      # "ghci" "haskeline"
+      "hpc"
+      "mtl" "parsec" "process" "text" "time" "transformers"
+      "unix" "xhtml"
+      # "stm" "terminfo"
+     ];
+    }];
+  };
+
+in
+  pkgSet.config.hsPkgs

nix-tools/cabal.project
@@ -1,20 +1,13 @@
-index-state: 2020-01-10T00:00:00Z
+index-state: 2020-05-03T00:00:00Z
 
 packages: .
 
--- Needs https://github.com/input-output-hk/iohk-nix/commit/6a8c29117eff36ce975e02e01efc8b25d93fcb90#diff-6fb0c6517b547a8baf082d5d2d604842
--- to work with the data-dir issues when building components.
-
-constraints: hnix >= 0.6.1
-
 source-repository-package
     type: git
-    location: https://github.com/ElvishJerricco/hackage-db.git
-    tag: 84ca9fc75ad45a71880e938e0d93ea4bde05f5bd
-    --sha256: 0y3kw1hrxhsqmyx59sxba8npj4ya8dpgjljc21gkgdvdy9628q4c
+    location: https://github.com/hamishmack/hackage-db.git
+    tag: f1f528db7b02d90e9953297c716de5e046f3570f
+    --sha256: 0j86xkpndxn41sy2mchx029zc3isaxh61hdvnakpiflkcmfaji1w
 
-source-repository-package
-    type: git
-    location: https://github.com/galenhuntington/haskell-src-meta.git
-    tag: 109ee29d5fd0f4e23fdd2f80eb122d66341b64a9
-    --sha256: 08qw6y9br6fy3qkwl9v2kp38msprsq9v1ssym0fsnj2jm0vbnfrx
+-- hnix requires hnix-store-core < 0.2, however hnix-store-core-0.1 breaks
+-- plan construction.
+allow-newer: hnix-store-core

nix-tools/default.nix
@@ -1,13 +1,35 @@
 { haskellNixSrc ? builtins.fetchTarball {
-      url = "https://github.com/input-output-hk/haskell.nix/archive/a84e3b55c642e7aec57c9677158cf446d90e9759.tar.gz";
-      sha256 = "0qnchf0f8d5k6363mcrqv936wz6zljs06bjvg1i6xmkg31bdj48m";
+      url = "https://github.com/input-output-hk/haskell.nix/archive/f78841c2d05c6c686d3888131104360944464dc2.tar.gz";
+      sha256 = "1zwhskx9drd0266kcrxf5vzcgs9vfiiibzkc2h3yarfb2xl7185q";
     }
-, nixpkgs ? haskellNixSrc + "/nixpkgs"
-, pkgs ? import nixpkgs (import haskellNixSrc)
-, haskellCompiler ? "ghc865"
+, nixpkgs ? (import haskellNixSrc {}).sources.nixpkgs-default
+, pkgs ? import nixpkgs (import haskellNixSrc {}).nixpkgsArgs
+, haskellCompiler ? "ghc883"
 }:
-  pkgs.haskell-nix.cabalProject {
-    src = pkgs.haskell-nix.haskellLib.cleanGit { src = ./.; };
+let
+  project = pkgs.haskell-nix.cabalProject {
+    src = pkgs.haskell-nix.haskellLib.cleanGit { src = ./.; name = "nix-tools"; };
     ghc = pkgs.haskell-nix.compiler.${haskellCompiler};
+    modules = [{
+     nonReinstallablePkgs= [ "rts" "ghc-heap" "ghc-prim" "integer-gmp" "integer-simple" "base"
+      "deepseq" "array" "ghc-boot-th" "pretty" "template-haskell"
+      # ghcjs custom packages
+      "ghcjs-prim" "ghcjs-th"
+      "ghc-boot"
+      "ghc" "Win32" "array" "binary" "bytestring" "containers"
+      "directory" "filepath" "ghc-boot" "ghc-compact" "ghc-prim"
+      # "ghci" "haskeline"
+      "hpc"
+      "mtl" "parsec" "process" "text" "time" "transformers"
+      "unix" "xhtml"
+      # "stm" "terminfo"
+     ];
+    }];
+  };
+in
+  project // {
+    shell = project.shellFor {
+      tools = { cabal = "3.2.0.0"; };
+    };
   }
 

nix-tools/hackage2nix/Main.hs
@@ -6,7 +6,7 @@ module Main where
 import           Cabal2Nix
 import           Control.Applicative                      ( liftA2 )
 import           Control.Monad.Trans.State.Strict
-import           Crypto.Hash.SHA256                       ( hashlazy )
+import           Crypto.Hash.SHA256                       ( hash )
 import           Data.Aeson
 import           Data.Aeson.Types                         ( Pair )
 import           Data.Aeson.Encode.Pretty
@@ -73,10 +73,10 @@ main = do
   createDirectoryIfMissing False (out </> "hackage")
 
   for_ cabalFiles $ \(cabalFile, pname, path) -> do
-    gpd <- cabal2nix False MinimalDetails src $ InMemory Nothing pname $ BL.toStrict cabalFile
+    gpd <- cabal2nix False MinimalDetails src $ InMemory Nothing pname $ cabalFile
     writeFile (out </> path) $ show $ prettyNix gpd
 
-type GPDWriter = State (Seq (BL.ByteString, String, FilePath))
+type GPDWriter = State (Seq (BS.ByteString, String, FilePath))
 
 newtype ApplicativeMonoid f a = ApplicativeMonoid { unApplicativeMonoid :: f a }
 instance (Applicative f, Semigroup a) => Sem.Semigroup (ApplicativeMonoid f a) where
@@ -116,7 +116,7 @@ version2json pname vnum (U.VersionData { U.cabalFileRevisions, U.metaFile }) =
 revBindingJson
   :: PackageName
   -> Version
-  -> BL.ByteString
+  -> BS.ByteString
   -> Integer
   -> GPDWriter (Text, Value)
 revBindingJson pname vnum cabalFile revNum = do
@@ -127,7 +127,7 @@ revBindingJson pname vnum cabalFile revNum = do
       revName     = "r" <> fromString (show revNum)
       revPath     = "." </> "hackage" </> qualifiedName <.> "nix"
       prettyPname = fromPretty pname
-      cabalHash   = Base16.encode $ hashlazy cabalFile
+      cabalHash   = Base16.encode $ hash cabalFile
   modify' $ mappend $ Seq.singleton
     (cabalFile, prettyPname ++ ".cabal", revPath)
   return $ revName .= object

nix-tools/lib/Cabal2Nix.hs
@@ -6,8 +6,8 @@ module Cabal2Nix (cabal2nix, gpd2nix, Src(..), CabalFile(..), CabalFileGenerator
 
 import Distribution.PackageDescription.Parsec (readGenericPackageDescription, parseGenericPackageDescription, runParseResult)
 import Distribution.Verbosity (normal)
-import Distribution.Text (disp)
 import Distribution.Pretty (pretty)
+import Distribution.Utils.ShortText (fromShortText)
 import Data.Char (toUpper)
 import System.FilePath
 import Data.ByteString (ByteString)
@@ -225,24 +225,24 @@ applyMkDefault :: NExpr -> NExpr
 applyMkDefault expr = mkSym pkgs @. "lib" @. "mkDefault" @@ expr
 
 instance ToNixExpr PackageIdentifier where
-  toNix ident = mkNonRecSet [ "name"    $= mkStr (fromString (show (disp (pkgName ident))))
-                            , "version" $= mkStr (fromString (show (disp (pkgVersion ident))))]
+  toNix ident = mkNonRecSet [ "name"    $= mkStr (fromString (show (pretty (pkgName ident))))
+                            , "version" $= mkStr (fromString (show (pretty (pkgVersion ident))))]
 
 toNixPackageDescription :: Bool -> CabalDetailLevel -> PackageDescription -> NExpr
 toNixPackageDescription isLocal detailLevel pd = mkNonRecSet $
-    [ "specVersion" $= mkStr (fromString (show (disp (specVersion pd))))
+    [ "specVersion" $= mkStr (fromString (show (pretty (specVersion pd))))
     , "identifier"  $= toNix (package pd)
     , "license"     $= mkStr (fromString (show (pretty (license pd))))
 
-    , "copyright"   $= mkStr (fromString (copyright pd))
-    , "maintainer"  $= mkStr (fromString (maintainer pd))
-    , "author"      $= mkStr (fromString (author pd))
+    , "copyright"   $= mkStr (fromString (fromShortText (copyright pd)))
+    , "maintainer"  $= mkStr (fromString (fromShortText (maintainer pd)))
+    , "author"      $= mkStr (fromString (fromShortText (author pd)))
 
-    , "homepage"    $= mkStr (fromString (homepage pd))
-    , "url"         $= mkStr (fromString (pkgUrl pd))
+    , "homepage"    $= mkStr (fromString (fromShortText (homepage pd)))
+    , "url"         $= mkStr (fromString (fromShortText (pkgUrl pd)))
 
-    , "synopsis"    $= mkStr (fromString (synopsis pd))
-    , "description" $= mkStr (fromString (description pd))
+    , "synopsis"    $= mkStr (fromString (fromShortText (synopsis pd)))
+    , "description" $= mkStr (fromString (fromShortText (description pd)))
 
     , "buildType"   $= mkStr (fromString (show (pretty (buildType pd))))
     ] ++
@@ -288,7 +288,7 @@ mkPrivateHackageUrl :: String -> PackageIdentifier -> String
 mkPrivateHackageUrl hackageUrl pi' =
   hackageUrl <> "/package/" <> pkgNameVersion <> "/" <> pkgNameVersion <> ".tar.gz"
   where
-    pkgNameVersion = unPackageName (pkgName pi') <> "-" <> show (disp (pkgVersion pi'))
+    pkgNameVersion = unPackageName (pkgName pi') <> "-" <> show (pretty (pkgVersion pi'))
 
 newtype SysDependency = SysDependency { unSysDependency :: String } deriving (Show, Eq, Ord)
 newtype BuildToolDependency = BuildToolDependency { unBuildToolDependency :: PackageName } deriving (Show, Eq, Ord)
@@ -302,7 +302,7 @@ toNixGenericPackageDescription isLocal detailLevel gpd = mkNonRecSet
                           , "package"       $= toNixPackageDescription isLocal detailLevel (packageDescription gpd)
                           , "components"    $= components ]
     where _packageName :: IsString a => a
-          _packageName = fromString . show . disp . pkgName . package . packageDescription $ gpd
+          _packageName = fromString . show . pretty . pkgName . package . packageDescription $ gpd
           component :: IsComponent comp => UnqualComponentName -> CondTree ConfVar [Dependency] comp -> Binding NExpr
           component unQualName comp
             = quoted name $=
@@ -396,13 +396,13 @@ instance ToNixExpr CompilerFlavor where
 
 instance ToNixExpr (VersionRangeF VersionRange) where
   toNix AnyVersionF              = mkBool True
-  toNix (ThisVersionF       ver) = mkSym "compiler" @. "version" @. "eq" @@ mkStr (fromString (show (disp ver)))
-  toNix (LaterVersionF      ver) = mkSym "compiler" @. "version" @. "gt" @@ mkStr (fromString (show (disp ver)))
-  toNix (OrLaterVersionF    ver) = mkSym "compiler" @. "version" @. "ge" @@ mkStr (fromString (show (disp ver)))
-  toNix (EarlierVersionF    ver) = mkSym "compiler" @. "version" @. "lt" @@ mkStr (fromString (show (disp ver)))
-  toNix (OrEarlierVersionF  ver) = mkSym "compiler" @. "version" @. "le" @@ mkStr (fromString (show (disp ver)))
+  toNix (ThisVersionF       ver) = mkSym "compiler" @. "version" @. "eq" @@ mkStr (fromString (show (pretty ver)))
+  toNix (LaterVersionF      ver) = mkSym "compiler" @. "version" @. "gt" @@ mkStr (fromString (show (pretty ver)))
+  toNix (OrLaterVersionF    ver) = mkSym "compiler" @. "version" @. "ge" @@ mkStr (fromString (show (pretty ver)))
+  toNix (EarlierVersionF    ver) = mkSym "compiler" @. "version" @. "lt" @@ mkStr (fromString (show (pretty ver)))
+  toNix (OrEarlierVersionF  ver) = mkSym "compiler" @. "version" @. "le" @@ mkStr (fromString (show (pretty ver)))
   toNix (WildcardVersionF  _ver) = mkBool False
---  toNix (MajorBoundVersionF ver) = mkSym "compiler" @. "version" @. "eq" @@ mkStr (fromString (show (disp ver)))
+--  toNix (MajorBoundVersionF ver) = mkSym "compiler" @. "version" @. "eq" @@ mkStr (fromString (show (pretty ver)))
   toNix (IntersectVersionRangesF v1 v2) = toNix (projectVersionRange v1) $&& toNix (projectVersionRange v2)
   toNix x = error $ "ToNixExpr VersionRange for `" ++ show x ++ "` not implemented!"
 

nix-tools/lib/Stack2nix.hs
@@ -28,7 +28,8 @@ import Data.Text.Prettyprint.Doc.Render.Text (hPutDoc)
 import Distribution.Types.PackageId (PackageIdentifier(..))
 import Distribution.Nixpkgs.Fetch (DerivationSource(..), Source(..), Hash(..), fetch)
 import Distribution.Simple.Utils (shortRelativePath)
-import Distribution.Text (Text(..), simpleParse)
+import Distribution.Text (simpleParse)
+import Distribution.Pretty (Pretty(..))
 
 import Cabal2Nix hiding (Git)
 import qualified Cabal2Nix as C2N
@@ -112,8 +113,8 @@ extraDeps2nix pkgs =
      | (PackageIdentifier pkg ver, (Just (Right revNo))) <- extraDeps ]
   where parsePackageIdentifier :: String -> Maybe PackageIdentifier
         parsePackageIdentifier = simpleParse
-        toText :: Text a => a -> T.Text
-        toText = fromString . show . disp
+        toText :: Pretty a => a -> T.Text
+        toText = fromString . show . pretty
 
 -- | Converts 'PackageFlags' into @{ packageName = { flags = { flagA = BOOL; flagB = BOOL; }; }; }@
 flags2nix :: PackageFlags -> [Binding NExpr]

commit d08782eca3a1f43d85d0a7d09f45bd71b13cdf9c

Fix @rev:<n> extra-deps specification not working (#90)
This mistake was introduced in 862036531a4efc61d5f3f64f8d56c4edd4661fe3.
Where previously the (+++) operator from
https://hackage.haskell.org/package/Cabal-2.4.1.0/docs/Distribution-Compat-ReadP.html#v:-43--43--43-
was used to give no precedence to left or right,
now the <|> operator is used instead, which fails if the left fails and
won't try the right (non-backtracking).

To fix this, a try is used,
which explicitly makes the code backtrack if the left was unsuccessful.

A better fix would be possible by reading until ':', then change
behavior depending on whether "sha256" or "rev" was read

nix-tools/lib/Stack2nix/Stack.hs
@@ -163,7 +163,7 @@ revSuffix :: ParsecParser CabalRev
 revSuffix = string "@rev:" *> (CabalRev . read <$> some (satisfy (`elem` ['0'..'9'])))
 
 suffix :: ParsecParser (Maybe (Either Sha256 CabalRev))
-suffix = option Nothing (Just <$> ((Left <$> sha256Suffix) <|> (Right <$> revSuffix)))
+suffix = option Nothing (Just <$> (try (Left <$> sha256Suffix) <|> (Right <$> revSuffix)))
 
 pkgIndex :: ParsecParser Dependency
 pkgIndex = PkgIndex <$> parsec <*> suffix <* eof